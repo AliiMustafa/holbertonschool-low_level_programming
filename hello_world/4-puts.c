db73a4c4a0ff23451d95d1cea05b3debd343ed12
root <aliimustafazada@gmail/com>
2024-06-22 10:27:10 +0000

    b

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-	char* str1="\"Programming is like building a multilingual puzzle";
-	puts(str1);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
