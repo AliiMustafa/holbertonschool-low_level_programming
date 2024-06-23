8b64933dc523fe71b898878e2fd96295d6583fbc
root <aliimustafazada@gmail.com>
2024-06-23 07:51:58 +0000

    *char

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - entry point
+ *
+ * Return: if
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int a = '0';
+	char *c = ",";
+	char *ch = " ";
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+		putchar(*c);
+		putchar(*ch);
+	}
+	putchar('\n');
+	return (0);
+}
