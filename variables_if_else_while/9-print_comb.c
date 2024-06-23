984cb726646e57763dcb641b4671f7a90bae326d
root <aliimustafazada@gmail.com>
2024-06-23 08:08:23 +0000

    *char

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,18 +9,20 @@
 int main(void)
 {
 	int a = '0';
-	char *c = ", ";
+	char *c = ",";
+	char *ch = " ";
 
 	for (a = '0'; a <= '9'; a++)
 	{
+		putchar(a);
 		if (a == '9')
 		{
-			putchar(a);
+			continue;
 		}
 		else
 		{
-			putchar(a);
 			putchar(*c);
+			putchar(*ch);
 		}
 	}
 	putchar('\n');
