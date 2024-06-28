e5405ecf5c5b44cbdbddfaf09b98d0e8e058197a
root <aliimustafazada@gmail.com>
2024-06-28 10:10:37 +0000

    print _

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - Main entry
+ *
+ *@n: varibale
+ * Return: print n times '_'
+ */
+void print_line(int n)
+{
+	int b;
+
+	if (n > 0)
+	{
+		while (n >= b)
+		{
+			_putchar('_');
+			b++;
+		}
+	}
+	_putchar('\n');
+}
