d4df1710b503b03b192fe43166aceb98ca08aa73
root <aliimustafazada@gmail.com>
2024-06-28 11:30:52 +0000

    square for size

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - Main entry
+ *
+ *@size: varibale
+ * Return: print square of size
+ */
+void print_square(int size)
+{
+	int n;
+	int m;
+
+	if (size > 0)
+	{
+		for (n = 0; n < size; n++)
+		{
+			_putchar('\n');
+			for (m = 0; m < size; m++)
+			{
+				_putchar('#');
+			}
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
