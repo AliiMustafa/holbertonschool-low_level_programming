e3c9dbc1c5cf72da792a530935f5ea16de5fca9b
root <aliimustafazada@gmail.com>
2024-06-30 15:20:30 +0000

    sal

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _atoi - Main entry
+ * @s: pointer
+ * Return: takes the integers from the string
+ */
+int _atoi(char *s)
+{
+	int i;
+	char temp = "0123456789";
+	char plus = "+";
+	char minus = "-";
+
+	i = 0;
+	while (*(s + i) != 0)
+	{
+		i++;
+	}
+	m = i - 1;
+	i = 0;
+	
+}
