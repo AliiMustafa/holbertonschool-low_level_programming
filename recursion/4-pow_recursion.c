23b49a9f625780f390e3f4ecaa580a5677c274e6
root <aliimustafazada@gmal.com>
2024-07-05 15:22:31 +0000

    pow

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -12,9 +12,13 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	if (y == 1 || y == 0)
+	if (y == 1)
 	{
 		return (x);
+	}
+	if (y == 0)
+	{
+		return (1);
 	}
 
 	return (x * _pow_recursion(x, (y - 1)));
