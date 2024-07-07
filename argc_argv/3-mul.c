cc153319c4a05b5cc3f8ec9a92d56915d2d92b67
root <aliimustafazada@gmail.com>
2024-07-07 14:48:52 +0000

    mul

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Main Entry
+ * @argc: varaible
+ * @argv: pointer
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int p = 1;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		p = p * atoi(argv[i]);
+	}
+	printf("%d\n", p);
+	return (0);
+}
