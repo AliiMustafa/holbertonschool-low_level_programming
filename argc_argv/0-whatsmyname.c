15779b1a56a5ffc81697baaa803a1cf2fe09db88
root <aliimustafazada@gmail.com>
2024-07-07 14:31:54 +0000

    argv

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,12 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
 
-/*
- * main - entry point
+/**
+ * main - Main Entry
  * @argc: varaible
  * @argv: pointer
- * Return: name
+ * Return: always 0
  */
 int main(int argc, char *argv[])
 {
