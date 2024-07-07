8d9615fcdaa04c71cd3a99e3eaf9374f26457662
root <aliimustafazada@gmail.com>
2024-07-07 14:38:09 +0000

    number of command

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Main Entry
+ * @argc: varaible
+ * @argv: pointer
+ * Return: always 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
