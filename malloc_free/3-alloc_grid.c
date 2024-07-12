97eb0b613411d6b09e908d79d1953108fa450c3b
AliiMustafa <aliimustafazada.com>
2024-07-12 18:59:42 +0400

    2 dimensional

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- *alloc grid - function
+ *alloc_grid - function
  *@width: variable
  *@height: variable
  *Return: cretae 2 dimensional array
@@ -12,20 +12,28 @@ int **alloc_grid(int width, int height)
 	int **t;
 	int i;
 	int j;
-	int size = height * width;
 
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
-	t = malloc(sizeof(int*) * size);
+	t = malloc(sizeof(int *) * height);
 	if (t == NULL)
 	{
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
 	{
-		for(j = 0; j < width; j++)
+		t[i] = malloc(sizeof(int) * width);
+		if (!t[i])
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(t[j]);
+			}
+			free(t);
+		}
+		for (j = 0; j < width; j++)
 		{
 			t[i][j] = 0;
 		}
