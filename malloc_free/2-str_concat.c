46c9acc6e5cac09d777ce22523e4eb7c27ccc6e2
AliiMustafa <aliimustafazada.com>
2024-07-12 17:31:05 +0400

    concat

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *str_concat - function
+ *@s1: pointer
+ *@s2: pointer
+ *Return: concatenate 2 string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *t;
+	int i = 0;
+	int j = 0;
+
+	while (*(s1 + i))
+	{
+		i++;
+	}
+	while (*(s2 + j))
+	{
+		j++;
+	}
+	t = malloc(sizeof(char) * (i + j));
+	if (t == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	j = 0;
+	while (*(s1 + i))
+	{
+		t[i] = s1[i];
+		i++;
+	}
+	while (*(s2 + j))
+	{
+		t[i] = s2[j];
+		i++;
+		j++;
+	}
+	t[i] = '\0';
+	return (t);
+}
