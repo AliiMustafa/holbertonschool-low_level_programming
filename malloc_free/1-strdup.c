71d56868bece775069e814d3109c82a84deefc25
AliiMustafa <aliimustafazada.com>
2024-07-12 16:46:24 +0400

    dublicate

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *_strdup - function
+ *@str: pointer
+ *Return: string
+ */
+
+
+char *_strdup(char *str)
+{
+	char *t;
+	int i = 0;
+	int j = 0;
+
+	while (*(str + j))
+	{
+		j++;
+	}
+	j = j - 1;
+	t = malloc(sizeof(char) * j);
+	if (t == NULL)
+	{
+		return (NULL);
+	}
+	while (*(str + i))
+	{
+		t[i] = str[i];
+		i++;
+	}
+	return (t);
+}
