1dee9838ef550cd6298cda3b35b021a873fc5038
AliiMustafa <aliimustafazada.com>
2024-07-13 21:30:48 +0400

    initialize

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "doh.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * init_dog - function
+ * @d: pointer
+ * @name: pointer
+ * @age: variable
+ * @owner: pointer
+ * Retrun: initialize
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
