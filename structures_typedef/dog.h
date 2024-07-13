0443070d8f601de85a9b9da750a5a07ee098f7d8
AliiMustafa <aliimustafazada.com>
2024-07-13 21:21:14 +0400

    struct

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog structure
+ * @name: pointer
+ * @age: pointer
+ * @owner: pointer
+ * Description: do structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
+
