c603874eeaeb9304e05e3ab28b51009eafc42f6e
AliiMustafa <aliimustafazada.com>
2024-07-18 21:58:03 +0400

    time_table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * times_table - function
+ * Return: table
+ */
+void times_table(void)
+{
+	int i, j, result, tam, kesr;
+	char *v = ",";
+	char *b = " ";
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (j == 9)
+			{
+				result = i * j;
+				tam = result / 10;
+				kesr = result % 10;
+				if (tam == 0)
+				{
+					_putchar('0' + kesr), _putchar('\n');
+				}
+				else
+				{
+					_putchar('0' + tam), _putchar('0' + kesr), _putchar('\n');
+				}
+			}
+			else
+			{
+				result = i * j;
+				tam = result / 10;
+				kesr = result % 10;
+				if (tam == 0)
+				{
+					_putchar('0' + kesr), _putchar(*v), _putchar(*b);
+				}
+				else
+				{
+					_putchar('0' + tam), _putchar('0' + kesr);
+					_putchar(*v), _putchar(*b);
+				}
+			}
+		}
+	}
+}
