d410269d58e093ea32d7df6ee18ff164c0232209
AliiMustafa <aliimustafazada.com>
2024-07-18 22:52:32 +0400

    print to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i == 98)
+			{
+				printf("%d", i);
+			}
+			else
+			{
+				printf("%d, ", i);
+			}
+		}
+	}
+	if (n == 98)
+	{
+		i = 98;
+		printf("%d", i);
+	}
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i == 98)
+			{
+				printf("%d", i);
+			}
+			else
+			{
+				printf("%d, ", i);
+			}
+		}
+	}
+	printf("\n");
+}
