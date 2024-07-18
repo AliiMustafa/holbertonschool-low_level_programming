187bc484ab7779d34d1963b96f331123cc7a398a
AliiMustafa <aliimustafazada.com>
2024-07-18 22:41:30 +0400

    add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *int_add - function
+ *@a: variable
+ *@b: variable
+ *Retrun: sum
+ *Retrun: 0
+ */
+
+int add(int a, int b)
+{
+	int sum = 0;
+
+	sum = a + b;
+	return (sum);
+	return (0);
+}
