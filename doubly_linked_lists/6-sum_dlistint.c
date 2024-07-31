043257236dcd34626a4ede82ead2f27e8d64e273
root <aliimustafazada@gmail.com>
2024-07-31 19:28:50 +0000

    sum of the elements of the list

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * sum_dnodeint - function
+ * @head: pointer
+ * Return: 0
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *t;
+	int sum = 0;
+
+	t = head;
+	if (t == NULL)
+	{
+		return (0);
+	}
+	while (t != NULL)
+	{
+		sum = sum + t->n;
+		t = t->next;
+	}
+	return (sum);
+}
