99b7d160707d4fea8a55fb6582d47a6da0345986
root <aliimustafazada.com>
2024-07-31 09:50:24 +0000

    add note

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * add_note - function
+ * @head: double pointer
+ * Return: pointer to list_t
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	int n = 0;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	while (str[n])
+	{
+		n++;
+	}
+	new->str = strdup(str);
+	new->len = n;
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
