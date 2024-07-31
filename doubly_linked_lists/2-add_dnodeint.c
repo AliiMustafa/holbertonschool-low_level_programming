895a0699af7a32f2628e25355b055a2fb217a89d
root <aliimustafazada@gmail.com>
2024-07-31 18:42:39 +0000

    add node

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -16,6 +16,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	{
 		return (NULL);
 	}
+	new->n = n;
 	if (!(*head))
 	{
 		new->prev = NULL;
@@ -23,7 +24,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		*head = new;
 		return (*head);
 	}
-	new->n = n;
 	(*head)->prev = new;
 	new->next = *head;
 	new->prev = NULL;
