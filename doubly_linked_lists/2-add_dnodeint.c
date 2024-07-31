5ae4a16f2d92c2132de7a10a09f5f69b973e43fa
root <aliimustafazada@gmail.com>
2024-07-31 18:33:00 +0000

    add node

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - pointer
+ * @head: double pointer
+ * @n: variable
+ * Return: pointer
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
