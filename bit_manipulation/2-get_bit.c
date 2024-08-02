eb1b8cc14e8b66244fe65e4271276f755f98457b
root <aliimustafazada.com>
2024-08-02 12:21:03 +0000

    the value of a bit at a given index

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * get_bit - function
+ * @n: variable
+ * @index: variable
+ * Return: the value of a bit at a givven index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int a = 0;
+	int q;
+
+	while (n)
+	{
+		if (a == index)
+		{
+			q = n % 2;
+			return (q);
+		}
+		n = n / 2;
+		a++;
+	}
+	return (-1);
+}
