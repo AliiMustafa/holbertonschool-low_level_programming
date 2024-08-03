f0c59f254710781b0470f663555eccf269238320
root <aliimustafazada.com>
2024-08-03 17:48:30 +0000

    read

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * read_textfile - function
+ * @filename: pointer
+ * @letters: variable
+ * Return: 0 or number of letters
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t Read, Write;
+	char *b;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	b = malloc(sizeof(char) * letters);
+	if (b == NULL)
+	{
+		return (0);
+	}
+	Read = read(fd, b, letters);
+	close(fd);
+	Write = write(1, b, letters);
+	free(b);
+	if (Read == -1 || Read != Write)
+	{
+		return (0);
+	}
+	return (Write);
+}
