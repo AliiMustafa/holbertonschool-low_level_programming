7732ff080cf34796c1739612aba6482f337ba901
root <aliimustafazada.com>
2024-08-04 12:35:03 +0000

    cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -7,7 +7,8 @@
   */
 int main(int ac, char *av[])
 {
-	int fd_from, fd_to, rd_stat, wr_stat;
+	int fd_from, fd_to;
+	ssize_t rd_stat, wr_stat;
 	mode_t perm = S_IRUSR | S_IWUSR | S_IWGRP | S_IRGRP | S_IROTH;
 	char buffer[1024];
 
@@ -19,21 +20,16 @@ int main(int ac, char *av[])
 	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, perm);
 	if (fd_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
-	rd_stat = 1;
-	while (rd_stat)
-	{
-		rd_stat = read(fd_from, buffer, 1024);
-		if (rd_stat == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-		if (rd_stat > 0)
-		{
-			wr_stat = write(fd_to, buffer, rd_stat);
-			if (wr_stat != rd_stat || wr_stat == -1)
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
-		}
-	}
+	rd_stat = read(fd_from, buffer, 1024);
+	close(fd_from);
 	if (close(fd_from) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+	if (rd_stat == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	wr_stat = write(fd_to, buffer, rd_stat);
+	if (wr_stat != rd_stat || wr_stat == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	close(fd_to);
 	if (close(fd_to) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
 	return (0);
