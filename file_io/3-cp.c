722be3d9f0e3a3e2f3e24e1ab80f34c668cb9af0
root <aliimustafazada.com>
2024-08-04 13:04:41 +0000

    cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -23,12 +23,12 @@ int main(int ac, char *av[])
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-	rd_stat = read(fd_from, buffer, 1024);
-	if (rd_stat == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, perm);
 	if (fd_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	rd_stat = read(fd_from, buffer, 1024);
+	if (rd_stat == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	wr_stat = write(fd_to, buffer, rd_stat);
 	if (wr_stat != rd_stat || wr_stat == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
