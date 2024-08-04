851b08a15d16b23ef33bddc4ffe76fd18d9dec30
root <aliimustafazada.com>
2024-08-04 12:52:56 +0000

    cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -10,13 +10,18 @@ int main(int ac, char *av[])
 	int fd_from, fd_to;
 	ssize_t rd_stat, wr_stat;
 	mode_t perm = S_IRUSR | S_IWUSR | S_IWGRP | S_IRGRP | S_IROTH;
-	char buffer[1024];
+	char *buffer;
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	buffer = malloc(sizeof(char) * 1024);
+	if (buffer == NULL)
+	{
+		return (0);
+	}
 	rd_stat = read(fd_from, buffer, 1024);
 	if (rd_stat == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
@@ -24,6 +29,7 @@ int main(int ac, char *av[])
 	if (fd_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	wr_stat = write(fd_to, buffer, rd_stat);
+	free(buffer);
 	if (wr_stat != rd_stat || wr_stat == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	if (close(fd_from) == -1)
